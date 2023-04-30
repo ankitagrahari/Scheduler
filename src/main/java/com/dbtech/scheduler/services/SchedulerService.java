4c6a27e81c9844cb674c13fbb77b53a3afeeaa13
Ankit Agrahari <ankitagrahari.rkgit@gmail.com>
2023-04-30 12:28:59 +0530

    update .gitignore

--- src/main/java/com/dbtech/scheduler/services/SchedulerService.java
+++ src/main/java/com/dbtech/scheduler/services/SchedulerService.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
